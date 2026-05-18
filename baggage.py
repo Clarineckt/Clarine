40a5a6f8f94e3f212ee8f09d4cff8672ea2faea7
clarineckt <rinkorinclarine26@gmail.com>
2026-05-18 14:46:22 +0700

    test: green test carry on

--- baggage.py
+++ baggage.py
@@ -7,4 +7,6 @@ def validate_baggage(baggage_weight, baggage_type, passenger_class, flight_type,
     if flight_type == "international" and not passport_valid:
         return False
         
-    return True
+    if baggage_type == "carry-on" and baggage_weight > 7:
+        return False
+    return True # Done Clarine
