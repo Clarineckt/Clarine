a3e23255c0c263315af2aaefdf8792a2ffb61af9
clarineckt <rinkorinclarine26@gmail.com>
2026-05-18 14:47:04 +0700

    Refactor: adding comment

--- baggage.py
+++ baggage.py
@@ -9,4 +9,4 @@ def validate_baggage(baggage_weight, baggage_type, passenger_class, flight_type,
         
     if baggage_type == "carry-on" and baggage_weight > 7:
         return False
-    return True # Done Clarine
+    return True # Done Clarine Refactor
