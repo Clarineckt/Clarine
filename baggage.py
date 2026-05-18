705877be3f39d88cd49d35eb72d431e47ea22aaf
michaelkt <michael.tanzil@binus.edu>
2026-05-18 14:19:27 +0700

    test: add failing tests for hazardous items and passport validation

---
+++ baggage.py
@@ -0,0 +1,3 @@
+def validate_baggage(baggage_weight, baggage_type, passenger_class, flight_type, hazardous_item, passport_valid):
+    # Kerangka awal, fungsi ini akan dilengkapi secara bertahap lewat TDD di masing-masing branch
+    return False
